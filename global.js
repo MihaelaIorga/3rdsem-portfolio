eb9d755c8cee07c49279ee6c6453f8832fb5551f
MihaelaIorga <mihaelalaura.iorga@gmail.com>
2018-03-01 17:30:44 +0100

    active-links

---
+++ global.js
@@ -0,0 +1,23 @@
+function removeActive(link) {
+    link.classList.remove('active')
+}
+document.querySelector(".clickwork").onclick = function (e) {
+    document.querySelectorAll('nav a').forEach(removeActive);
+    e.target.classList.add('active');
+}
+
+document.querySelector(".clickabout").onclick = function (e) {
+    document.querySelectorAll('nav a').forEach(removeActive);
+    e.target.classList.add('active');
+}
+
+
+document.querySelector(".clickresume").onclick = function (e) {
+    document.querySelectorAll('nav a').forEach(removeActive);
+    e.target.classList.add('active');
+}
+
+document.querySelector(".clickcontact").onclick = function (e) {
+    document.querySelectorAll('nav a').forEach(removeActive);
+    e.target.classList.add('active');
+}

b8449d4fb4636c4011f002c326697f7a896bea2e
MihaelaIorga <mihaelalaura.iorga@gmail.com>
2018-04-24 01:12:34 +0200

    tools bar

--- resume.js
+++ resume.js
@@ -6,3 +6,44 @@ function myClick() {
         x.style.display = "none";
     }
 }
+
+
+
+
+
+//skills bar
+
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round($elem.offset().top);
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.bars');
+
+    // If the animation has already been started
+    if ($elem.hasClass('start')) return;
+
+    if (isElementInViewport($elem)) {
+
+        console.log('isInView')
+        // Start the animation
+        $elem.addClass('start');
+    }
+}
+
+// Capture scroll events
+$(window).scroll(function () {
+    checkAnimation();
+});
